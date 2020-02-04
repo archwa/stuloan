8adf2c75ae8291053d107f8c427113a58dcf7ed5
Arthur C. Watkins <archwa@protonmail.ch>
2020-02-03 21:59:26 -0600

    added graphing capabilities with matplotlib (need python3-tk)

--- calc.py
+++ calc.py
@@ -5,6 +5,15 @@ import math
 from datetime import datetime
 from datetime import timedelta
 from pprint import pprint
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+months = mdates.MonthLocator((1, 4, 7, 10))
+weeks = mdates.YearLocator()
+months_fmt = mdates.DateFormatter('%b\'%y')
+
 
 class Calculation:
   def __init__(self, params):
@@ -30,9 +39,15 @@ class Calculation:
 
     paid = 0
 
+    balance_dates = [ ]
+    payment_dates = [ ]
+    payments = [ ]
+    balances = [ ]
+
     while True:
       
       if projected_date.day == payment_day:
+        payment_dates.append(projected_date)
         paid += payment_amount
 
         # apply payment
@@ -40,15 +55,20 @@ class Calculation:
 
         if updated_accrued_interest >= 0:
           accrued_interest = updated_accrued_interest
+          payments.append(paid)
         else:
           accrued_interest = 0
           updated_principal = math.ceil((principal + updated_accrued_interest) * 100.0) / 100.0
           if updated_principal > 0:
             principal = updated_principal
+            payments.append(paid)
           else: # done paying
             balance = principal + accrued_interest
             principal = 0
             paid -= abs(updated_principal)
+            payments.append(paid)
+            balance_dates.append(projected_date)
+            balances.append(balance)
 
             print('Projected to complete payment by', projected_date.strftime('%Y-%m-%d'))
             print(f'You will have paid a total of ${paid:.2f}.')
@@ -57,6 +77,8 @@ class Calculation:
       
       balance = math.ceil((principal + accrued_interest)*100.0) / 100.0 # update balance
 
+      balance_dates.append(projected_date)
+      balances.append(balance)
       projected_date += timedelta(days=1) # increment day
 
       # update days in year after new year
@@ -64,7 +86,30 @@ class Calculation:
         days_in_year = (datetime(projected_date.year + 1, 1, 1) - datetime(projected_date.year, 1, 1)).days
       
       accrued_interest = math.ceil((accrued_interest + (rate / days_in_year)*principal)*100.0) / 100.0  # accrue interest
-    
+
+    fig, ax = plt.subplots()
+    ax.plot(balance_dates, balances, label='Balance')
+    ax.plot(payment_dates, payments, 'ro', label='Payment Total')
+    ax.xaxis.set_major_locator(months)
+    ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('${x:,.2f}'))
+    ax.xaxis.set_major_formatter(months_fmt)
+    ax.xaxis.set_minor_locator(weeks)
+    ax.set_xlim(balance_dates[0], balance_dates[-1])
+    ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('USD')
+    ax.set_title('Projected Student Loan Repayment', fontsize=14, fontweight='bold')
+
+    starting_date = balance_dates[0].strftime("%d %b'%y")
+    ax.annotate(f'Starting Balance (${balances[0]:,.2f} on {starting_date})', xy=(balance_dates[0], balances[0]), xytext=(balance_dates[round(len(balance_dates)*.04)], 1.07*balances[0]), arrowprops=dict(facecolor='black', shrink=0.005, width=2), fontweight='bold')
+    final_date = payment_dates[-1].strftime("%d %b'%y")
+    ax.annotate(f'Grand Total (${payments[-1]:,.2f} on {final_date})', xy=(payment_dates[-1], payments[-1]), xytext=(payment_dates[round(len(payment_dates)*.74)], 0.73*payments[-1]), arrowprops=dict(facecolor='black', shrink=0.005, width=2), fontweight='bold')
+
+    ax.legend(loc='upper center')
+    ax.grid(True)
+    fig.autofmt_xdate()
+    plt.show()
+    plt.savefig('mygraph.png')
 
 
 try:
